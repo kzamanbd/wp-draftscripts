426a7c7a31ed06e8da52728d4e0279b4e3a04a93
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-15 21:21:30 +0600

    chore: added custom fields

--- classes/Actions/Settings.php
+++ classes/Actions/Settings.php
@@ -4,15 +4,53 @@ namespace WpDraftScripts\Actions;
 
 class Settings
 {
+    /**
+     * @var array $pages
+     */
     public array $pages = [];
+
+    /**
+     * @var array $subPages
+     */
     public array $subPages = [];
 
+    /**
+     * @var array $customFields
+     */
+    public array $customFields = [];
+
+    /**
+     * @var array $sections
+     */
+
+    public array $sections = [];
+
+    /**
+     * @var array $fields
+     */
+    public array $fields = [];
+
+
+    /**
+     * Register the actions
+     * @return void
+     */
     public function register()
     {
         if (!empty($this->pages)) {
             add_action('admin_menu', array($this, 'addAdminMenu'));
         }
+
+        if (!empty($this->customFields)) {
+
+            add_action('admin_init', array($this, 'registerCustomFields'));
+        }
     }
+
+    /**
+     * Add the admin menu
+     * @return void
+     */
 
     public function addAdminMenu()
     {
@@ -40,11 +78,24 @@ class Settings
         }
     }
 
+    /**
+     * Add the pages
+     * @param array $pages
+     * @return $this
+     */
+
     public function addPages(array $pages)
     {
         $this->pages = $pages;
         return $this;
     }
+
+    /**
+     * Add the sub pages
+     * @param array $pages
+     * @param string|null $title
+     * @return $this
+     */
 
     public function withSubPage(array $pages = [], string $title = null)
     {
@@ -66,6 +117,113 @@ class Settings
         ];
 
         $this->subPages = array_merge($subPage, $pages);
+
+        return $this;
+    }
+
+    /**
+     * Add the customFields
+     * @param array $customFields
+     * @return $this
+     */
+    public function addCustomFields(array $customFields)
+    {
+        $this->customFields = $customFields;
+        return $this;
+    }
+
+    /**
+     * Register the customFields
+     * @return $this
+     */
+
+    public function registerCustomFields()
+    {
+        if (empty($this->customFields)) {
+            return $this;
+        }
+
+        foreach ($this->customFields as $setting) {
+            register_setting(
+                $setting['option_group'],
+                $setting['option_name'],
+                $setting['callback'] ?? null
+            );
+        }
+
+        $this->registerSections();
+        $this->registerFields();
+
+        return $this;
+    }
+
+    /**
+     * Add the sections
+     * @param array $sections
+     * @return $this
+     */
+
+    public function addSections(array $sections)
+    {
+        $this->sections = $sections;
+        return $this;
+    }
+
+    /**
+     * Register the sections
+     * @return $this
+     */
+
+    public function registerSections()
+    {
+        if (empty($this->sections)) {
+            return $this;
+        }
+        foreach ($this->sections as $section) {
+            add_settings_section(
+                $section['id'],
+                $section['title'],
+                $section['callback'],
+                $section['page']
+            );
+        }
+        return $this;
+    }
+
+    /**
+     * Add the fields
+     * @param array $fields
+     * @return $this
+     */
+
+    public function addFields(array $fields)
+    {
+        $this->fields = $fields;
+        return $this;
+    }
+
+    /**
+     * register the fields
+     * @param array $fields
+     * @return $this
+     */
+
+    public function registerFields()
+    {
+        if (empty($this->fields)) {
+            return $this;
+        }
+
+        foreach ($this->fields as $field) {
+            add_settings_field(
+                $field['id'],
+                $field['title'],
+                $field['callback'],
+                $field['page'],
+                $field['section'] ?? 'default',
+                $field['args'] ?? null
+            );
+        }
 
         return $this;
     }
--- classes/Services/AdminServices.php
+++ classes/Services/AdminServices.php
@@ -7,23 +7,65 @@ use WpDraftScripts\Support\BaseApplication;
 
 class AdminServices extends BaseApplication
 {
+    /**
+     * @var Settings $settings
+     */
     public $settings;
 
+    /**
+     * @var array $pages
+     */
     public array $pages = [];
+
+    /**
+     * @var array $subPages
+     */
+
     public array $subPages = [];
+
+    /**
+     * @var array $customFields
+     */
+
+    public array $customFields = [];
+
+    /**
+     * @var array $sections
+     */
+
+    public array $sections = [];
+
+    /**
+     * @var array $fields
+     */
+
+    public array $fields = [];
+
+
+    /**
+     * AdminServices constructor.
+     */
 
     public function __construct()
     {
         parent::__construct();
         $this->settings = new Settings();
     }
-
+    /**
+     * Register the actions
+     * @return void
+     */
     public function register()
     {
 
         $this->setPages();
+        $this->setCustomFields();
+
         $this->settings->addPages($this->pages)
             ->withSubPage($this->subPages, 'General')
+            ->addCustomFields($this->customFields)
+            ->addSections($this->sections)
+            ->addFields($this->fields)
             ->register();
     }
 
@@ -48,7 +90,7 @@ class AdminServices extends BaseApplication
                 'menu_title' => 'CPT',
                 'capability' => 'manage_options',
                 'menu_slug' => 'draftscripts-cpt',
-                'callback' => array($this, 'customPost')
+                'callback' => array($this, 'settings')
             ],
             [
                 'parent_slug' => 'draftscripts',
@@ -56,9 +98,64 @@ class AdminServices extends BaseApplication
                 'menu_title' => 'Settings',
                 'capability' => 'manage_options',
                 'menu_slug' => 'draftscripts-settings',
-                'callback' => array($this, 'customPost')
+                'callback' => array($this, 'settings')
             ]
 
+        ];
+    }
+
+    /**
+     * Set the custom fields
+     * @return void
+     */
+    public function setCustomFields()
+    {
+        $this->customFields = [
+            [
+                'option_group' => 'draftscripts_options_group',
+                'option_name' => 'text_example',
+                'callback' => array($this, 'addedOptionsGroup'),
+            ],
+            [
+                'option_group' => 'draftscripts_options_group',
+                'option_name' => 'full_name',
+            ]
+        ];
+
+        $this->sections = [
+            [
+                'id' => 'draftscripts_index',
+                'title' => 'Settings',
+                'callback' => array($this, 'addedSectionGroup'),
+                'page' => 'draftscripts'
+            ]
+        ];
+
+        $this->fields = [
+            [
+                'id' => 'text_example',
+                'title' => 'Text Example',
+                'callback' => array($this, 'addedField'),
+                'section' => 'draftscripts_index',
+                'page' => 'draftscripts',
+                'args' => [
+                    'label_for' => 'text_example',
+                    'class' => 'regular-text',
+                    'option_name' => 'text_example'
+                ]
+            ],
+            [
+                'id' => 'full_name',
+                'title' => 'Full Name',
+                'callback' => array($this, 'addedField'),
+                'section' => 'draftscripts_index',
+                'page' => 'draftscripts',
+                'args' => [
+                    'label_for' => 'full_name',
+                    'class' => 'regular-text',
+                    'option_name' => 'full_name'
+                ]
+            ]
         ];
     }
 
@@ -69,8 +166,35 @@ class AdminServices extends BaseApplication
         ]);
     }
 
-    public function customPost()
+    public function settings()
     {
         $this->view('settings');
+    }
+
+    public function addedOptionsGroup($input)
+    {
+        return $input;
+    }
+
+    public function addedSectionGroup()
+    {
+        echo 'Set your settings here';
+    }
+
+    public function addedField($args)
+    {
+        $name = $args['label_for'];
+        $class = $args['class'];
+        $option_name = $args['option_name'];
+        $value = esc_attr(get_option($option_name));
+        $type = isset($args['type']) ? $args['type'] : 'text';
+        $placeholder = isset($args['placeholder']) ? $args['placeholder'] : 'Write something here';
+        switch ($type) {
+            case 'textarea':
+                echo "<textarea id='$name' name='$option_name' class='$class' placeholder='$placeholder'>$value</textarea>";
+                break;
+            default:
+                echo "<input type='text' id='$name' name='$option_name' value='$value' class='$class' placeholder='$placeholder'>";
+        }
     }
 }
